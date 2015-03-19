ccdb4147d3e1bda3f5f82b7b4aee95a6c780d07e
Edward Frias <feek1g@gmail.com>
2015-03-18 20:32:50 -0700

    work in progress

--- rubyChallenge/caeser_cipher_p1.rb
+++ rubyChallenge/caeser_cipher_p1.rb
@@ -1,6 +1,25 @@
 #create a cipher shifts the output n times.
 
 def caesar_cipher sentence, num
+  upper_letters = ("A".ord.."Z".ord).to_a
+  lower_letters = ("a".ord.."z".ord).to_a
+  output = ""
+
+  text.split("").each do |char|
+    new_char = char
+    if char.scan(/\w/).length > 0
+      if char.upcase == char
+        new_char_idx = (upper_letters.index(char.ord) + num) % 26
+        new_char = upper_letters[new_char_idx].chr
+      else
+        new_char_idx = (lower_letters.index(char.ord) + num) % 26
+        new_char = lower_letters[new_char_idx].chr
+      end
+    end
+    output += new_char
+  end
+
+  output 
 end
 
 p caesar_cipher("What a string!", 5)

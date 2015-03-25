80fc30cd4b8b6f098eec75a33ab3154ef9749e80
Edward Frias <feek1g@gmail.com>
2015-03-25 03:34:57 -0700

    on2 solution for stock picker

--- rubyChallenge/stock_picker.rb
+++ rubyChallenge/stock_picker.rb
@@ -2,13 +2,22 @@
 # You need to buy before you sell
 # Example stock_picker([17,3,6,9,15,8,6,1,10]) => [1,4]  # for a profit of $15 - $3 == $12
 
+#brute force solution
 def stock_picker stocks
-  max_profit = 0
-  best_buy_date = 0
-  best_sell_date = 0
-  present_day = 0
+  buy_date, sell_date, max_profit = 0, 0, 0
 
-
+  stocks[0...-1].each_with_index do |buy, day|
+    stocks[(day+1)..-1].each do |sell|
+      
+      if sell - buy > max_profit
+        max_profit = sell - buy
+        buy_date = stocks.index(buy)
+        sell_date = stocks.index(sell)
+      end
+      
+    end
+  end
+  [buy_date, sell_date]
 end
 
-stock_picker([17,3,6,9,15,8,6,1,10])
+p stock_picker([17,3,6,9,15,8,6,1,10])

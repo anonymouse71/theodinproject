944bdc417da0b25489d4c37c912b5993324c17fa
Edward Frias <feek1g@gmail.com>
2015-03-25 20:04:54 -0700

    refactored select, and added my_all? and my_any? methods

--- rubyChallenge/enumerable.rb
+++ rubyChallenge/enumerable.rb
@@ -9,7 +9,6 @@ module Enumerable
   end
 
   def my_each_with_index
-    i = 0
     c = 0
     while i < self.length
       yield self[i], c
@@ -22,15 +21,30 @@ module Enumerable
   def my_select
     i = 0
     truthy_results = []
-    while i < self.length
-      truthy_results << self[i] if yield(self[i])
-      i += 1
+    self.my_each do |i|
+      truthy_results << i if yield(i)
     end
     truthy_results
+  end
+
+  def my_all?
+    self.my_each do |i|
+      return false unless yield(i)
+    end
+    return true
+  end
+
+  def my_any?
+    self.my_each do |i|
+      return true unless yield(i)
+    end
+    return false
   end
 
 end
 
 #p [1,2,3].my_each {|x| p x}
 #p ['a','b','c'].my_each_with_index {|l, index| p "#{index}. #{l}"}
-p (1..5).to_a.my_select {|x| x % 2 == 0}
+#p (1..5).to_a.my_select {|x| x % 2 == 0}
+#p %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
+#p %w[ant bear cat].my_any? { |word| word.length >= 4 } #=> true

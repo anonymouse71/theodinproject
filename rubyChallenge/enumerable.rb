e7326b05d6f945af80b74bfd1f6cfb32a8231ed4
Edward Frias <feek1g@gmail.com>
2015-03-25 17:53:24 -0700

    added each_with_index and select methods

--- rubyChallenge/enumerable.rb
+++ rubyChallenge/enumerable.rb
@@ -8,6 +8,29 @@ module Enumerable
     self
   end
 
+  def my_each_with_index
+    i = 0
+    c = 0
+    while i < self.length
+      yield self[i], c
+      i += 1
+      c += 1
+    end
+    self
+  end
+
+  def my_select
+    i = 0
+    truthy_results = []
+    while i < self.length
+      truthy_results << self[i] if yield(self[i])
+      i += 1
+    end
+    truthy_results
+  end
+
 end
 
-p [1,2,3].my_each {|x| p x}
+#p [1,2,3].my_each {|x| p x}
+#p ['a','b','c'].my_each_with_index {|l, index| p "#{index}. #{l}"}
+p (1..5).to_a.my_select {|x| x % 2 == 0}

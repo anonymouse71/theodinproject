e8b42c800c0b0e5502db0b990495432e41184c1c
Edward Frias <feek1g@gmail.com>
2015-04-01 08:35:57 -0700

    Made final changes and game is working!

--- rubyChallenge/tic_tac_toe.rb
+++ rubyChallenge/tic_tac_toe.rb
@@ -37,13 +37,14 @@ module TicTacToe
     def winner?
       winning_values = ["xxx", "ooo"]
       winning_positions.each do |rows|
-        return true if winning_values.include?(rows.join)
+        string = rows.map{|cell| cell.value}.join
+        return true if winning_values.include?(string)
       end
       false
     end
     def winning_positions
-      grid
-      grid.transpose
+      grid+
+      grid.transpose+
       diagonals
     end
 
@@ -60,8 +61,68 @@ module TicTacToe
   end
 end
 
-b = TicTacToe::Board.new
+module TicTacToe
+  class Game
+    attr_reader :board, :current_player, :other_player
+    def initialize players
+      @players = players
+      @board = Board.new
+      @current_player, @other_player = @players.shuffle
+    end
+
+    def switch_players
+      @current_player, @other_player = @other_player, @current_player
+    end
+
+    def make_move
+      "#{current_player.name}: Enter a number from 1 and 9 to make your move."
+    end
+
+    def get_move move = gets.chomp
+      player_coordinates(move)
+    end
+
+    def player_coordinates player_move
+      maps = {
+        "1"=> [0,0],
+        "2"=> [0,1],
+        "3"=> [0,2],
+        "4"=> [1,0],
+        "5"=> [1,1],
+        "6"=> [1,2],
+        "7"=> [2,0],
+        "8"=> [2,1],
+        "9"=> [2,2]
+      }
+      maps[player_move]
+    end
+
+    def game_over_message
+      return "#{current_player.name} won!" if board.game_over == :winner
+      return "Draw!" if board.game_over == :draw
+    end
+
+    def play
+      puts "#{current_player.name} has been selected to go first."
+      while true
+        board.print_board
+        puts
+        puts make_move
+        x,y = get_move
+        board.set_cell(x,y, current_player.sym)
+        if board.game_over
+          puts game_over_message
+          board.print_board
+          exit
+        else
+          switch_players
+        end
+      end
+    end
+  end
+end
 e = TicTacToe::Player.new "ed", "x"
 m = TicTacToe::Player.new "mel", "o"
-b.print_board
-p b.winner?
+players = [e,m]
+game = TicTacToe::Game.new players
+game.play

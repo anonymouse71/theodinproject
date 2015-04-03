aaafd92b0925d02a240d823ef810291c88a85f9c
Edward Frias <feek1g@gmail.com>
2015-04-03 13:05:24 -0700

    fixed zipcode issue.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,8 +1,16 @@
 require 'csv'
 puts "Event manager has been Initialized"
 
-contents = CSV.open "event_attendees.csv", headers: true
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
-  name = row[2]
-  p name
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+
+  zipcode = "00000" if zipcode.nil?
+  if zipcode.length < 5
+    zipcode.rjust 5, "0"
+  elsif zipcode.length > 5
+    zipcode[0..4]
+  end
+  p "#{name} from #{zipcode}"
 end

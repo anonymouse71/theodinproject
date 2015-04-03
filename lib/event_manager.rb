51d654daeae312a318655ce82ed961f417bb2d87
Edward Frias <feek1g@gmail.com>
2015-04-03 12:49:37 -0700

    adds csv library

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,7 +1,8 @@
+require 'csv'
 puts "Event manager has been Initialized"
 
-lines = File.readlines "event_attendees.csv"
-lines.each do |line|
-  columns = line.split(",")
-  p columns
+contents = CSV.open "event_attendees.csv", headers: true
+contents.each do |row|
+  name = row[2]
+  p name
 end

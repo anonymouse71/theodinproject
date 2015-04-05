cd97debe3d8f91fe3635d3942ff049eba12469e2
Edward <feek1g@gmail.com>
2015-04-04 21:07:55 -0700

    adds guess, and play function

--- Hangman/hangman.rb
+++ Hangman/hangman.rb
@@ -1,11 +1,12 @@
 module Hangman
   class Game
-    attr_reader :board
+    attr_reader :board, :secret_word, :missed_words, :guess_count
     def initialize
       @file = File.open("5desk.txt", "r"){|file| file.read}
       @valid_words = get_valid_words! @file
       @guess_count = 0
-      @board = Array.new(secret_word.length){"_"}
+      @secret_word = secret_word
+      @board = Array.new(@secret_word.length){"_"}
       @missed_words = []
     end
 
@@ -14,20 +15,29 @@ module Hangman
     end
 
     def secret_word
-      @valid_words.sample
+      @valid_words.sample.downcase
     end
 
-    def guess
-      @guess_count += 1
+    def guess!
       user_input = View.get_guess
+      if @secret_word.include?(user_input)
+        @board[@secret_word.index(user_input)] = user_input
+      else
+        @missed_words << user_input
+        @guess_count +=1
+      end
     end
 
     def play!
       View.greeting
+      return View.congratulations! if @board.none?{|c|c == "_"}
       until @guess_count > 9
         View.render_board @board
-        guess
+        View.render_missed_words @missed_words
+        View.render_guess_count @guess_count
+        guess!
       end
+      View.game_over! @secret_word
     end
   end
 end
@@ -40,8 +50,27 @@ module Hangman
       puts
     end
 
+    def self.congratulations!
+      puts "Ding Ding! We have a winner! Thanks for playing..."
+    end
+
+    def self.game_over! secret_word
+      puts "Sorry you lose! the word was #{secret_word}"
+    end
+
     def self.render_board game_board
+      puts "\e[H\e[2J"
       game_board.each {|char| print "#{char} "}
+      puts
+    end
+
+    def self.render_missed_words missed_words
+      puts
+      puts "Incorrect Letter: #{ missed_words.join(", ") }"
+    end
+
+    def self.render_guess_count guess_count
+      puts  "Chances left: #{10-guess_count}"
     end
 
     def self.get_guess

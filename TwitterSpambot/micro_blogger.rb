69d0cea8617431ca5fdcca248a3e52515024738e
Edward <feek1g@gmail.com>
2015-04-06 20:57:12 -0700

    adds run function

--- TwitterSpambot/micro_blogger.rb
+++ TwitterSpambot/micro_blogger.rb
@@ -16,7 +16,22 @@ class MicroBlogger
       puts "error: invalid message!"
     end
   end
+
+  def run
+    puts "Welcome to the JSL Twitter Client!"
+    command = ""
+    while command != 'q'
+      printf "Enter command:> "
+      command = gets.chomp
+
+      case command
+      when 'q' then puts "Goodbye"
+      else puts "Sorry '#{command}' is not a valid command!"
+      end
+    end
+  end
 end
 
-microblog = MicroBlogger.new
-
+  microblog = MicroBlogger.new
+  #microblog.tweet "MicroBlogger has been initialized. This message has been sent via JumpStarts' API."
+  microblog.run

1b21d06141be353c0c339b8fa28f23e636817542
Edward <feek1g@gmail.com>
2015-04-07 05:22:43 -0700

    added readme and finished twitterbot

---
+++ TwitterSpambot/README.md
@@ -0,0 +1,32 @@
+## Twitter Spambot
+
+#### Microblogger tutorial from Jumpstart Labs
+
+In this multi-phase project, you will build a client that interacts with the Twitter messaging service. Your client will both mimic functionality found through the twitter.com web interface as well as perform many new tasks.
+
+Learning Goals:
+
+* Practice installing and using a gem library
+* Writing methods and basic instructions
+* Practice basic techniques including conditional branching and looping
+
+Sample output:
+
+```
+Initializing...
+Welcome to the JSL Twitter Client!
+**************************************************
+Type 'menu' for a list of all available commands
+Enter command:> menu
+Menu Commands!
+--------------------------------------------------
+q - quit
+t (message) - sends a tweet with whatever message you like.
+dm (person) (message) - sends a direct message to a person.
+sf (message) - spams all your followers with message.
+latest - shows everyones recent tweets
+surl - shorten a url
+turl - Tweet a message + url, and bitly will shorten it
+--------------------------------------------------
+Enter command:>
+```
--- TwitterSpambot/micro_blogger.rb
+++ TwitterSpambot/micro_blogger.rb
@@ -1,4 +1,5 @@
 require 'jumpstart_auth'
+require 'bitly'
 
 class MicroBlogger
   attr_reader :client
@@ -29,6 +30,8 @@ class MicroBlogger
 
   def run
     puts "Welcome to the JSL Twitter Client!"
+    puts "*"*50
+    puts "Type 'menu' for a list of all available commands"
     command = ''
     while command != 'q'
       printf "Enter command:> "
@@ -37,14 +40,30 @@ class MicroBlogger
       command = parts[0]
 
       case command
+      when 'menu' then print_menu
       when 'q' then puts "Goodbye"
       when 't' then tweet parts[1..-1].join(" ")
       when 'dm' then dm parts[1], parts[2..-1].join(" ")
       when 'sf' then spam_my_followers parts[1..-1].join(" ")
       when 'latest' then everyones_latest_tweet
+      when 'surl' then shorten parts[1]
+      when 'turl' then tweet(parts[1..-2].join(" ") + " " + shorten(parts[-1]))
       else puts "Sorry '#{command}' is not a valid command!"
       end
     end
+  end
+
+  def print_menu
+    puts "Menu Commands!"
+    puts "-"*50
+    puts "q - quit"
+    puts "t (message) - sends a tweet with whatever message you like."
+    puts "dm (person) (message) - sends a direct message to a person."
+    puts "sf (message) - spams all your followers with message."
+    puts "latest - shows everyones recent tweets"
+    puts "surl - shorten a url"
+    puts "turl - Tweet a message + url, and bitly will shorten it"
+    puts "-"*50
   end
 
   def follower_list
@@ -59,7 +78,8 @@ class MicroBlogger
   end
 
   def everyones_latest_tweet
-    friends = @client.friends.sort_by {|friend| friend.screen_name.downcase}
+    friends = []
+    @client.followers.each {|follower| friends.push(@client.user(follower))}
 
     friends.each do |friend|
       timestamp = friend.status.created_at
@@ -72,8 +92,14 @@ class MicroBlogger
     end
   end
 
+  def shorten original_url
+    Bitly.use_api_version_3
+    bitly = Bitly.new('hungryacademy', 'R_430e9f62250186d2612cca76eee2dbc6')
+    puts "Shortening this URL: #{original_url}"
+    bitly.shorten(original_url).short_url
+  end
+
 end
 
 microblog = MicroBlogger.new
-#microblog.tweet "MicroBlogger has been initialized. This message has been sent via JumpStarts' API."
 microblog.run

46b1c9d494f84bd9db377c78bd549ea6ea0e26b3
Edward <feek1g@gmail.com>
2015-04-14 15:58:08 -0700

    fixed typo

--- RubyOnRails/restClient/rest_client.rb
+++ RubyOnRails/restClient/rest_client.rb
@@ -1,8 +1,9 @@
 require 'rest_client'
 require 'nokogiri'
+
 puts "Below are all the cool animes from Manga Panda:"
 url = "http://www.mangapanda.com/alphabetical"
 page = Nokogiri::HTML(RestClient.get(url))
 
 anime_list = page.css("a")
-anime_list.eacitem {|item| puts item.text}
+anime_list.each {|item| puts item.text}

04b5bde5d2ff0f15f8dd46dcfc943c715b29fbd7
Edward <feek1g@gmail.com>
2015-04-17 12:50:02 -0700

    associations have been completed

--- RubyOnRails/micro-reddit/app/models/comment.rb
+++ RubyOnRails/micro-reddit/app/models/comment.rb
@@ -1,2 +1,10 @@
 class Comment < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :post
+
+  validates :title, presence: true
+  validates :title, length: { maximum:20 }
+
+  validates :body, presence: true
+  validates :body, length: { maximum:140 }
 end
--- RubyOnRails/micro-reddit/db/schema.rb
+++ RubyOnRails/micro-reddit/db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150417182202) do
+ActiveRecord::Schema.define(version: 20150417185954) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "title"
+    t.string   "body"
+    t.integer  "post_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text     "title"
